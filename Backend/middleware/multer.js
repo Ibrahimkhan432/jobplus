5edc6fa371c7abb0c7470b462d239cd2f4c8ec22
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-09 17:15:56 +0500

    job plus client side separated

--- Backend/middleware/multer.js
+++
@@ -1,7 +0,0 @@
-import multer from 'multer';
-const storage = multer.memoryStorage();
-
-export const profileAndResumeUpload = multer({ storage }).fields([
-  { name: "profilePhoto", maxCount: 1 },
-  { name: "file", maxCount: 1 }
-]);
